1a07fab01a82acb2662d1b691ce77d879ab4bd55
aadhirai718 <vvarshini0706@gmail.com>
2026-03-29 15:16:52 +0530

    Update models.py

--- backend/app/database/models.py
+++ backend/app/database/models.py
@@ -1,1 +1,17 @@
+from app.database.db import get_connection
 
+def create_table():
+    conn = get_connection()
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS logs (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        question TEXT,
+        state TEXT,
+        strategy TEXT
+    )
+    """)
+
+    conn.commit()
+    conn.close()
