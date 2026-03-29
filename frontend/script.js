25ee3f49a31b674d9aa45ea67ec9f9b8405bfed6
aadhirai718 <vvarshini0706@gmail.com>
2026-03-29 15:06:11 +0530

    Update script.js

--- frontend/script.js
+++ frontend/script.js
@@ -1,1 +1,15 @@
+async function send() {
+    let res = await fetch("http://127.0.0.1:8000/learn", {
+        method: "POST",
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            question: document.getElementById("q").value,
+            answer: document.getElementById("a").value,
+            time_taken: Math.random()*30
+        })
+    });
 
+    let data = await res.json();
+    document.getElementById("out").innerText =
+        JSON.stringify(data, null, 2);
+}
